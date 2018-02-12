80d5e4d87ffe951f40d48a2946b6265544d27eba
Oscar Evertsson <oscar.evertsson@live.com>
2018-02-12 10:00:41 +0100

    Created method headers and basic check for some obvious bad requests.

--- WebServer.java
+++ WebServer.java
@@ -5,9 +5,10 @@
 //
 
 import java.io.*;
-import java.net.*;
-import java.util.*;
-import java.net.InetAddress.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public final class WebServer {
     public static void main(String argv[]) throws Exception {
@@ -39,6 +40,8 @@ public final class WebServer {
     }
 }
 
+
+
 final class HttpRequest implements Runnable {
     // Constants
     //   Recognized HTTP methods
@@ -48,6 +51,28 @@ final class HttpRequest implements Runnable {
         final static String POST = "POST";
     }
 
+
+
+    enum HTTP_STATUS_CODE {
+        OK, BAD_REQUEST, NOT_FOUND, INTERNAL_SERVER_ERROR, NOT_IMPLEMENTED;
+
+        public static String toString(HTTP_STATUS_CODE code) {
+            switch (code) {
+                case OK:
+                    return "200 OK";
+                case BAD_REQUEST:
+                    return "400 Bad Request";
+                case NOT_FOUND:
+                    return "404 Not Found";
+                case INTERNAL_SERVER_ERROR:
+                    return "500 Internal Server Error";
+                default:
+                    return "501 Not Implemented";
+            }
+        }
+    }
+
+    final static int MIN_VALID_PARTS = 3;
     final static String HTTPVERSION = "HTTP/1.0";
     final static String CRLF = "\r\n";
     Socket socket;
@@ -74,8 +99,7 @@ final class HttpRequest implements Runnable {
 
         // Set up input stream filters
         BufferedReader br = new BufferedReader(new InputStreamReader(ins));
-
-
+        
         // Get the request line of the HTTP request
         String requestLine = br.readLine();
 
@@ -83,11 +107,65 @@ final class HttpRequest implements Runnable {
         System.out.println();
         System.out.println("Request:");
         System.out.println("  " + requestLine);
+        //Handles the response
+        String result = handleRequest(requestLine);
+        System.out.println(result);
 
         // Close streams and sockets
         outs.close();
         br.close();
         socket.close();
+    }
+
+    private static String handleRequest(String request) {
+        String[] spaces = request.split(" ");
+
+        //BAD REQUEST
+        if (spaces.length < MIN_VALID_PARTS) {
+            return badRequest();
+        }
+
+        String method = spaces[0];
+        String argument = spaces[1];
+        String version = spaces[2];
+        switch (method) {
+            case HTTP_METHOD.GET:
+                return handleGet(argument, version);
+            case HTTP_METHOD.HEAD:
+                return handleHead(argument, version);
+            case HTTP_METHOD.POST:
+                return handlePost();
+            default:
+                return badRequest();
+        }
+    }
+
+    private static String handleGet(String argument, String version) {
+        return "";
+    }
+
+    private static String handleHead(String argument, String version) {
+        return "";
+    }
+
+    private static String handlePost() {
+        return (HTTPVERSION + " ") + HTTP_STATUS_CODE.toString(HTTP_STATUS_CODE.NOT_IMPLEMENTED) + "\n" +
+                displayDate();
+    }
+
+    private static String displayDate() {
+        // s z y
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EE MMM d H:m:s z y");
+        ZonedDateTime d = ZonedDateTime.now();
+        String s = d.format(formatter);
+        return "Date: " + s;
+    }
+
+    private static String badRequest() {
+        StringBuilder b = new StringBuilder();
+        b.append(HTTPVERSION + " ").append(HTTP_STATUS_CODE.toString(HTTP_STATUS_CODE.BAD_REQUEST)).append("\n");
+        b.append(displayDate());
+        return b.toString();
     }
 
     private static void sendBytes(FileInputStream fins,
